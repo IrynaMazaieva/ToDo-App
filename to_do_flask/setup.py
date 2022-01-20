ffe35d1a84c58a34b422bdd60bef693be32d8b51
imazaieva <ira.gosudarstvo@gmail.com>
2022-01-20 13:34:51 +0200

    Added setup.py.

---
+++ to_do_flask/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='ToDo-App',
+    version='0.1.0',
+    packages=['logic'],
+    package_dir={'to_do_flask': 'to_do_flask'},
+    url='http://127.0.0.1:5000/',
+    license='',
+    author='imazaieva',
+    author_email='ira.gosudarstvo@gmail.com',
+    description='my pet-project'
+)
